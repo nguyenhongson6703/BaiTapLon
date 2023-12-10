9e023ba1eef1143f8c053fe9a401662f2cbb6828
nguyenhongson6703 <hongson06072003@gmail.com>
2023-12-10 12:25:34 +0700

    first-command

---
+++ src/hello3/mainclass.java
@@ -0,0 +1,13 @@
+package hello3;
+
+public class mainclass {
+
+	public mainclass() {
+		
+		// TODO Auto-generated constructor stub
+	}
+	public static void main(String[] args) {
+		System.out.println("helloword");
+	}
+
+}
